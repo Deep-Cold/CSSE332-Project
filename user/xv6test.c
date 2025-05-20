762d71e91221e72b2ca21d0007f12956c86bb6c7
Deep-Cold <1164846816shs@gmail.com>
2025-05-20 11:21:21 -0400

    2025-5-20

--- user/xv6test.c
+++ user/xv6test.c
@@ -4,19 +4,19 @@
 #include "user/thread.h"
 
 void *myfunc(void) {
-    printf("Hello from myfunc\n");
     sleep(3);
     int x = 10;
     x += 10;
-    printf("%d\n", x);
+    printf("The value of 10 + 10 = %d\n", x);
     return 0;
 }
 
 void *myfunc2(void *argu) {
     sleep(2);
     int x = *(int*)argu;
+    printf("The value of argu is %d\n", x);
     x *= 20;
-    printf("%d\n", x);
+    printf("The value of argu * 20 = %d\n", x);
     return 0;
 }
 
@@ -66,6 +66,7 @@ void *myfunc3(void *argu) {
     int x = *(int*)argu;
     sleep(2);
     x *= 20;
+    printf("I am going to return argu * 20 = %d\n", x);
     int *ret = malloc(sizeof(int));
     *ret = x;
     return ret;
@@ -77,12 +78,12 @@ void *myfunc4(void) {
     sleep(20);
     for(int i = 0; i < 7; i++) {
         argu[i] = i;
-        pthread_create(&tid[i], getg, &argu[i]);
+        pthread_create(&tid[i], getf, &argu[i]);
     }
     for(int i = 0; i < 7; i++) {
         int *ret = 0;
         pthread_join(tid[i], &ret);
-        printf("The return value is %d\n", *ret);
+        printf("The return value of F[%d] = %d\n", i, *ret);
         free(ret);
     }
     return 0;
@@ -116,32 +117,50 @@ void *thread_t2(void *arg)
 
 int main(int argc, char *argv[]) {
     printf("Hello from my test case in xv6\n");
-    int tid3;
-    // int tid2;
-    int tid, tid2;
+    printf("\n");
     int argu = 100;
-    pthread_create(&tid, myfunc, 0);
-    printf("the tid of my child1 is %d\n", tid);
-    pthread_create(&tid2, myfunc2, &argu);
-    printf("the tid of my child2 is %d\n", tid2);
-    pthread_create(&tid3, myfunc3, &argu);
+    {
+        int tid, tid2;
+        printf("Testing for basic thread creation start: \n");
+        pthread_create(&tid, myfunc, 0);
+        pthread_create(&tid2, myfunc2, &argu);
+        pthread_join(tid, 0);
+        pthread_join(tid2, 0);
+        printf("the tid of my child1 is %d\n", tid);
+        printf("the tid of my child2 is %d\n", tid2);
+        printf("Testing for basic thread creation end. \n");
+    }
+    printf("\n");
     sleep(10);
-    pthread_join(tid, 0);
-    pthread_join(tid2, 0);
-    int *ret = 0;
-    pthread_join(tid3, &ret);
-    printf("The return value is %d\n", *ret);
-    free(ret);
-
-    int tid4;
-    pthread_create(&tid4, myfunc4, 0);
-    printf("the tid of my child4 is %d\n", tid4);
-    pthread_join(tid4, 0);
-
-    int pt1, pt2;
-    pthread_create(&pt1, thread_t1, 0);
-    pthread_create(&pt2, thread_t2, 0);
-    pthread_join(pt1, 0);
-    pthread_join(pt2, 0);
+    {
+        printf("Testing for thread with return value start: \n");
+        int tid3;
+        pthread_create(&tid3, myfunc3, &argu);
+        int *ret = 0;
+        pthread_join(tid3, &ret);
+        printf("The return value is %d\n", *ret);
+        free(ret);
+        printf("Testing for thread with return value end. \n");
+    }
+    printf("\n");
+    sleep(10);
+    {
+        printf("Testing for recursive function and recursive thread creation start: \n");
+        int tid4;
+        pthread_create(&tid4, myfunc4, 0);
+        pthread_join(tid4, 0);
+        printf("Testing for recursive function and recursive thread creation end. \n");
+    }
+    printf("\n");
+    sleep(10);
+    {
+        printf("Testing for sbrk memory sharing start: \n");
+        int pt1, pt2;
+        pthread_create(&pt1, thread_t1, 0);
+        pthread_create(&pt2, thread_t2, 0);
+        pthread_join(pt1, 0);
+        pthread_join(pt2, 0);
+        printf("Testing for sbrk memory sharing end. \n");
+    }
     exit(0);
 }
